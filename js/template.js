473ab53bbdc1a3a4055ce799f550441c3da92c91
marzoonobrega <marzoo.nobrega@gmail.com>
2017-09-20 10:14:46 +0200

    Final edit
    The Final working edit.

--- js/template.js
+++ js/template.js
@@ -1,7 +1,7 @@
 var= users= [];
 Var templates= [
- {id: 1, name: 'Light', value: "css/light.css"}
- {id: 1, name: 'Dark', value: "css/nightmode.css"}
+ {id: 1, name: 'Light', value: "css/light.scss"}
+ {id: 2, name: 'Dark', value: "css/nightmode.scss"}
 ]
 		
 	
